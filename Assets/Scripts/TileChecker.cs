24f2ec1deb106f1a7f95323fb5cc2a70052503f9
anicolauiesmanacor <anicolau@politecnicllevant.cat>
2023-11-08 23:24:23 +0100

    a
    a

--- Assets/Scripts/TileChecker.cs
+++ Assets/Scripts/TileChecker.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TileChecker : MonoBehaviour {
     private void OnCollisionEnter(Collision collision) {
-        Debug.Log("aaa");
         if (collision.gameObject.CompareTag("Tile")) {
             Debug.Log("Hit: " + collision.gameObject.name + "!");
         }
