0cce558a87e5c98f87b5848be0ed06e5bc9a7470
anicolauiesmanacor <anicolau@politecnicllevant.cat>
2023-11-11 21:26:43 +0100

    a
    a

--- Assets/Scripts/PlaceTheBomb.cs
+++ Assets/Scripts/PlaceTheBomb.cs
@@ -9,14 +9,30 @@ public class PlaceTheBomb : MonoBehaviour {
     GameObject newBomb;
 
     void Update() {
-        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,0,this.gameObject.transform.position.z);
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 5f, this.gameObject.transform.position.z);
+    
         if (Input.GetMouseButtonDown(0)) {
+            Ray ray = new Ray(this.gameObject.transform.position, Vector3.down);
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 1f);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.CompareTag("Block")) {
+                    Debug.Log("BLOCK");
+                } else if (hit.collider.CompareTag("Tile")) {
+                    newBomb = Instantiate(bomb, hit.collider.gameObject.transform.position, hit.collider.gameObject.transform.rotation);
+                    newBomb.GetComponent<Collider>().enabled = true;
+                    newBomb.transform.SetParent(hit.collider.gameObject.transform);    
+                }
+            }
+/*
             if (tileTarget.transform.childCount == 0) {
                 newBomb = Instantiate(bomb, tileTarget.transform.position, tileTarget.transform.rotation);
                 newBomb.GetComponent<Collider>().enabled = true;
                 newBomb.transform.SetParent(tileTarget.transform);
                 //StartCoroutine(DestroyBomb(newBomb, 5f));
             }
+*/
         }
     }
 
