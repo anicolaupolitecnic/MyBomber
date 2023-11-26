5dcbbf680e1251e0f7d4b2a15b5433197a3ab0de
anicolaupolitecnic <anicolau@politecnicllevant.cat>
2023-11-26 23:12:43 +0100

    a
    a

---
+++ Assets/Images/MenuAbout/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    /*
+     2D IMAGES
+     BOMB ICON    https://www.shutterstock.com/es/image-vector/fire-flames-pixel-icons-set-1474671566
+     FIRE ICON https://www.shutterstock.com/es/image-vector/bomb-pixel-icon-8bit-illustration-2225211633
+     LIVE ICON https://www.shutterstock.com/es/image-vector/red-pixel-heart-retro-video-games-1854350308
+     WOODEN BOX https://vilelarts.itch.io/woods-pack
+
+    3D MODELS
+    Medieval Dungeon https://quaternius.com/packs/medievaldungeon.html
+    Modular lDungeon https://quaternius.com/packs/medievaldungeon.html
+
+    SOUNDS
+    Cavern FX https://freesound.org/people/Arcana23/sounds/688003/
+    Menu Music https://freesound.org/people/audiomirage/sounds/690696/
+    Game Music https://freesound.org/people/audiomirage/sounds/690696/
+    Game FX Icon Grab https://freesound.org/people/mazk1985/sounds/187407/
+    Fx Game Explosion https://freesound.org/people/Iwiploppenisse/sounds/156031/
+
+    FONTS
+    DUNGEON https://www.dafont.com/es/dungeon-sn.font
+
+    EXTRAS
+
+     */
+}
