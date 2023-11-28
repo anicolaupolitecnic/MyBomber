6a9e6463191517ee292fb658685cccb9155226a2
anicolaupolitecnic <anicolau@politecnicllevant.cat>
2023-11-28 09:52:54 +0100

    a
    a

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyController : MonoBehaviour {
 
     private NavMeshAgent navMeshAgent;
     [SerializeField] private GameObject target;
+    private Ray ray;
+    RaycastHit hit;
 
     void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        ray = new Ray();
     }
 
     void Update() {
-        navMeshAgent.destination = target.transform.position;
+        //navMeshAgent.destination = target.transform.position;
+        Debug.DrawRay(ray.origin, ray.direction * 20, Color.red, Mathf.Infinity);
+        if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out hit, 2f) && hit.transform.tag == "Wall") {
+            Debug.Log("Wall front");
+        } else {
+            Debug.Log("Nothing front");
+        }
+
+        if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.back), out hit, 2f) && hit.transform.tag == "Wall") {
+            Debug.Log("Wall back");
+        } else {
+            Debug.Log("Nothing back");
+        }
     }
 }
